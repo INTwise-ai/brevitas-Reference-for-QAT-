b0de01332a1d3f83fd1abc0aaf854a5c4eb0056d
Builder_Ed <68166480+Builder-Ed@users.noreply.github.com>
2024-05-10 11:10:15 +0800

    Create intwise_ste[PreTest].py

---
+++ src/brevitas/function/intwise_ste[PreTest].py
@@ -0,0 +1,45 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+def clamp(x, a, b):
+    return np.maximum(a, np.minimum(b, x))
+
+
+a = -5
+b = 5
+
+
+def floor(x):
+    return clamp(np.floor(x), a, b)
+
+
+def sigmoid_floor(x, T=0):
+    y = (
+        np.exp(-T) * (x + 1)
+        - 1
+        + np.sum(
+            [
+                1 / (1 + np.exp(-T * (x - i))) - 0.5
+                for i in range(int(np.min(x)), int(np.max(x)))
+            ],
+            axis=0,
+        )
+    )
+    return clamp(y, a, b)
+
+def ste(x):
+    # return 1 if x in a and b, 0 otherwise
+    return np.logical_and(x >= a, x <= b).astype(np.float32)
+
+def sigmoid_estimator(x, T=0):
+    y = np.exp(-T) + np.sum(
+        [
+            np.exp(-T * (x - i)) * T / (1 + np.exp(-T * (x - i))) ** 2
+            for i in range(int(np.min(x)), int(np.max(x)))
+        ],
+        axis=0,
+    )
+    return y * ste(x)
+
+
+
